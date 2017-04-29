81b8cd3b89b5674fd9cff5ba0f96c0f172694514
Jay Ravaliya <jayrav13@gmail.com>
2017-04-29 16:54:57 -0400

    Updating seeder

--- database/seeds/RidesSeeder.php
+++ database/seeds/RidesSeeder.php
@@ -5,28 +5,28 @@ use App\Models\Ride;
 
 class RidesSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-    	ini_set('memory_limit', '-1');
-    	$data = explode("\n", Storage::disk("local")->get("result.csv"));
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		ini_set('memory_limit', '-1');
+		$data = explode("\n", Storage::disk("local")->get("result.csv"));
 
-    	for($i = 1; $i < count($data); $i++)
-    	{
-    		echo $data[$i] . "\n";
-    		$row = explode(",", $data[$i]);
-    		$ride = Ride::create([
-    			"timestamp" => $row[0],
-    			"latitude" => $row[1],
-    			"longitude" => $row[2],
-    			"base" => str_replace("\"", "", $row[3]),
-    			"name" => str_replace("\"", "", $row[4]),
-    			"type" => str_replace("\"", "", $row[5]),
-    		]);
-    	}
-    }
+		for($i = count($data); $i > 0; $i--)
+		{
+			echo $data[$i] . "\n";
+			$row = explode(",", $data[$i]);
+			$ride = Ride::create([
+				"timestamp" => $row[0],
+				"latitude" => $row[1],
+				"longitude" => $row[2],
+				"base" => str_replace("\"", "", $row[3]),
+				"name" => str_replace("\"", "", $row[4]),
+				"type" => str_replace("\"", "", $row[5]),
+			]);
+		}
+	}
 }
