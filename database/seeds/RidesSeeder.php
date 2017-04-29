26cb4a4a36ad5fd190edd5545723d76548e20010
Jay Ravaliya <jayrav13@gmail.com>
2017-04-29 19:48:05 -0400

    Proper query catcher

--- database/seeds/RidesSeeder.php
+++ database/seeds/RidesSeeder.php
@@ -17,29 +17,10 @@ class RidesSeeder extends Seeder
 
 		for($i = count($data) - 2; $i > 0; $i--)
 		{
-			echo $data[$i] . "\n";
-
-			$row = explode(",", $data[$i]);
-			$packet = [
-				"timestamp" => $row[0],
-				"latitude" => $row[1],
-				"longitude" => $row[2],
-				"base" => str_replace("\"", "", $row[3]),
-				"name" => str_replace("\"", "", $row[4]),
-				"type" => str_replace("\"", "", $row[5]),
-			];
-
-			$validator = Validator::make($packet, [
-				"timestamp" => "required|date",
-				"latitude" => "required|numeric",
-				"longitude" => "required|numeric",
-				"base" => "required",
-				"name" => "required",
-				"type" => "required",
-			]);
-
-			if($validator->fails())
+			try
 			{
+				echo $data[$i] . "\n";
+				$row = explode(",", $data[$i]);
 				$ride = Ride::create([
 					"timestamp" => $row[0],
 					"latitude" => $row[1],
@@ -49,9 +30,11 @@ class RidesSeeder extends Seeder
 					"type" => str_replace("\"", "", $row[5]),
 				]);
 			}
-			else
+			catch (\Illuminate\Database\QueryException $exception)
 			{
-
+				echo "FAIL";
+				continue;
+				// echo "FAIL: " . $data;
 			}
 		}
 	}
