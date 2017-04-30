1d27164509417d8f1d8d02c60d48e8b1ddd7821e
Jay Ravaliya <jayrav13@gmail.com>
2017-04-30 02:03:05 -0400

    Random order

--- app/Http/Controllers/RidesController.php
+++ app/Http/Controllers/RidesController.php
@@ -12,7 +12,7 @@ class RidesController extends Controller
 
 	public function index(Request $request)
 	{
-		$rides = Ride::take(15000)->get();
+		$rides = Ride::where("name", "Uber")->inRandomOrder()->take(15000)->get();
 		return Response::json($rides);
 	}
 
