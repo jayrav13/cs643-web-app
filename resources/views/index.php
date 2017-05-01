e9c3878e24e348dac4b4f0b9667627a800da7ba2
Jay Ravaliya <jayrav13@gmail.com>
2017-05-01 03:07:56 -0400

    Fixing UI

--- resources/views/index.php
+++ resources/views/index.php
@@ -54,7 +54,7 @@
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav">
 						<li><a href="/#!/peak/locations">Peak Locations</a></li>
-						<li><a href="/#!/pickups"></a></li>
+						<li><a href="/#!/pickups">Pickups by Date</a></li>
 					</ul>
 				</div><!-- /.navbar-collapse -->
 
